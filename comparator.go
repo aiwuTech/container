364b5a7df231b49ea34cda882bd37a33bd33757f
Mint-Zhao-Chiu <mint.zhao.chiu@gmail.com>
2015-08-27 17:14:17 +0800

    add string compare function

--- comparator.go
+++ comparator.go
@@ -68,3 +68,26 @@ func Int64CompareFunctionASC(e1, e2 interface{}) int8 {
 func Int64CompareFunctionDESC(e1, e2 interface{}) int8 {
 	return -Int64CompareFunctionASC(e1, e2)
 }
+
+func StringCompareFunction(a, b interface{}) int8 {
+	s1 := a.(string)
+	s2 := b.(string)
+	min := len(s2)
+	if len(s1) < len(s2) {
+		min = len(s1)
+	}
+	diff := 0
+	for i := 0; i < min && diff == 0; i++ {
+		diff = int(s1[i]) - int(s2[i])
+	}
+	if diff == 0 {
+		diff = len(s1) - len(s2)
+	}
+	if diff < 0 {
+		return -1
+	}
+	if diff > 0 {
+		return 1
+	}
+	return 0
+}
