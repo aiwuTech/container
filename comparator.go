aa4d8fc7223a6393a6be31709e3cc2e78143e335
Mint-Zhao-Chiu <mint.zhao.chiu@gmail.com>
2015-08-28 03:10:18 +0800

    better code

--- comparator.go
+++ comparator.go
@@ -13,13 +13,30 @@
 // under the License.
 package container
 
+import (
+	"reflect"
+)
+
 // compareFunc的结果值：
 //   小于0: 第一个参数小于第二个参数
 //   等于0: 第一个参数等于第二个参数
 //   大于1: 第一个参数大于第二个参数
 type CompareFunction func(interface{}, interface{}) int8
 
+func validElements(elements ...interface{}) bool {
+	for _, e := range elements {
+		if !reflect.ValueOf(e).IsValid() {
+			return false
+		}
+	}
+	return true
+}
+
 func Float64CompareFunctionASC(e1, e2 interface{}) int8 {
+	if !validElements(e1, e2) {
+		return 0
+	}
+
 	k1 := e1.(float64)
 	k2 := e2.(float64)
 
@@ -31,21 +48,20 @@ func Float64CompareFunctionASC(e1, e2 interface{}) int8 {
 		return 0
 	}
 }
+
 func Float64CompareFunctionDESC(e1, e2 interface{}) int8 {
 	return -Float64CompareFunctionASC(e1, e2)
 }
 
 func Uint64CompareFunctionASC(e1, e2 interface{}) int8 {
+	if !validElements(e1, e2) {
+		return 0
+	}
+
 	k1 := e1.(uint64)
 	k2 := e2.(uint64)
 
-	if k1 < k2 {
-		return -1
-	} else if k1 > k2 {
-		return 1
-	} else {
-		return 0
-	}
+	return Float64CompareFunctionASC(float64(k1), float64(k2))
 }
 
 func Uint64CompareFunctionDESC(e1, e2 interface{}) int8 {
@@ -53,25 +69,42 @@ func Uint64CompareFunctionDESC(e1, e2 interface{}) int8 {
 }
 
 func Int64CompareFunctionASC(e1, e2 interface{}) int8 {
+	if !validElements(e1, e2) {
+		return 0
+	}
+
 	k1 := e1.(int64)
 	k2 := e2.(int64)
 
-	if k1 < k2 {
-		return -1
-	} else if k1 > k2 {
-		return 1
-	} else {
-		return 0
-	}
+	return Float64CompareFunctionASC(float64(k1), float64(k2))
 }
 
 func Int64CompareFunctionDESC(e1, e2 interface{}) int8 {
 	return -Int64CompareFunctionASC(e1, e2)
 }
 
-func StringCompareFunction(a, b interface{}) int8 {
-	s1 := a.(string)
-	s2 := b.(string)
+func IntCompareFunctionASC(e1, e2 interface{}) int8 {
+	if !validElements(e1, e2) {
+		return 0
+	}
+
+	k1 := e1.(int)
+	k2 := e2.(int)
+
+	return Float64CompareFunctionASC(float64(k1), float64(k2))
+}
+
+func IntCompareFunctionDESC(e1, e2 interface{}) int8 {
+	return -IntCompareFunctionASC(e1, e2)
+}
+
+func StringCompareFunction(e1, e2 interface{}) int8 {
+	if !validElements(e1, e2) {
+		return 0
+	}
+
+	s1 := e1.(string)
+	s2 := e2.(string)
 	min := len(s2)
 	if len(s1) < len(s2) {
 		min = len(s1)
